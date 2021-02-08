526457983065b736abada71c3b6eee60bde6ae75
Yongjin Jo <69415375+yu04038@users.noreply.github.com>
2021-02-09 04:08:49 +0900

    배경음악 유지, level select scene에서는 유지 취소

---
+++ Assets/scripts/Audioscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Audioscript : MonoBehaviour
+{
+    private static Audioscript instance;
+
+    void Update() {
+        if (SceneManager.GetActiveScene().name == "Level Select") 
+        {
+            if(Input.GetMouseButtonDown(0)) {
+                Debug.Log(SceneManager.GetActiveScene().name);
+            }
+            Destroy(gameObject);
+        }
+    }
+    void Awake ()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject); 
+        }
+    }
+}
